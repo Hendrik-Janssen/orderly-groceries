306a534dcf39439bda98dbdbd1c10599ba8c97d7
Hendrik <hendrikjanssen29@gmail.com>
2021-05-13 13:36:57 +0200

    update dependencies, add bootstrap, add start page component

--- orderly-groceries-ui/src/app/app.module.ts
+++ orderly-groceries-ui/src/app/app.module.ts
@@ -5,12 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { StartPageComponent } from './start-page/start-page.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { StartPageModule } from './start-page/start-page.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    // Orderly Groceries imports
+    StartPageModule,
+
+    // Other imports
     BrowserModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
@@ -18,7 +25,8 @@ import { environment } from '../environments/environment';
       // Register the ServiceWorker as soon as the app is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
